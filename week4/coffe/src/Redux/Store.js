7f6dba3e1b468dd73bf76337322e1a10894f6968
JassinAlSafe <121439769+Cakewhale97@users.noreply.github.com>
2024-03-22 09:02:21 +0100

    update

--- week4/coffe/src/Redux/Store.js
+++ week4/coffe/src/Redux/Store.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice";
 
+// Export the configured store as the default export of this module.
 export default configureStore({
+  // Pass an object to the reducer option. Each key-value pair in this defines a slice of the redux state.
+  // The key is the name of the slice, and the value is the reducer function for that slice.
+  // In this case, theres a single slice named #"cart", and its reducer function is cartReducer. 
   reducer: {
     cart: cartReducer,
   },
--- week4/coffe/src/Router/Routes.jsx
+++ week4/coffe/src/Router/Routes.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Import the components for each of your routes
 import Hero from "../Hero";
 import Menu from "../Menu";
 import Cart from "../Cart"
 
+//Define a component for your apps router. 
 const AppRouter = () => {
     return (
+        // Wrap eveything in a Router component.  This sets up the context for routing. 
         <Router>
             <Routes>
                 <Route path="/" element={<Hero />} />
